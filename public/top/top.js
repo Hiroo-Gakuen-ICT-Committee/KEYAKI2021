bdfd63d2e8ec3345bf46db18d42aa06f6d0c3832
Tomoro0726 <tomoro0726@gmail.com>
2021-07-29 23:53:03 +0900

    First Commit

---
+++ public/top/top.js
@@ -0,0 +1,32 @@
+$('.menu').on('click', function () {
+  $('.menu__line').toggleClass('active');
+  $('.gnav').fadeToggle();
+  $('.day').fadeToggle();
+  //上記のコードはsection追加ごとに付け足す
+});
+
+//出てくるやつ
+ScrollReveal().reveal('.text', {
+  duration: 1600,
+  origin: 'left',
+  distance: '50px',
+  reset: true
+});
+ScrollReveal().reveal('.textimage img', {
+  duration: 1600,
+  origin: 'right',
+  distance: '50px',
+  reset: true
+});
+ScrollReveal().reveal('.sns', {
+  duration: 1600,
+  origin: 'left',
+  distance: '50px',
+  reset: true
+});
+ScrollReveal().reveal('.yazirusi', {
+  duration: 1600,
+  origin: 'right',
+  distance: '50px',
+  reset: true
+});
