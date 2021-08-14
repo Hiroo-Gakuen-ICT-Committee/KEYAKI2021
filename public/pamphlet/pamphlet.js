22c3b2b83e26810dffa77d00a77785fdcadddee5
Tomoro0726 <tomoro0726@gmail.com>
2021-08-15 00:43:47 +0900

    パンフレットダウンロードページ完成

--- public/pamphlet/pamphlet.js
+++ public/pamphlet/pamphlet.js
@@ -5,3 +5,15 @@ $('.menu').on('click', function () {
   //上記のコードはsection追加ごとに付け足す
   $('section').fadeToggle();
 });
+
+
+
+
+
+function mouseover() {
+  download_botan.innerHTML = "10MB/表紙+49ページ";
+}
+
+function mouseout() {
+  download_botan.innerHTML = "Download/10MB";
+}
