5ffbfa67afa6a7ee26f95f22f2749af5bc2e272d
Tomoro0726 <tomoro0726@gmail.com>
2021-08-14 23:20:14 +0900

    新規ページ生成

--- public/theme/theme.js
+++ public/theme/theme.js
@@ -5,3 +5,27 @@ $('.menu').on('click', function () {
   //上記のコードはsection追加ごとに付け足す
   $('section').fadeToggle();
 });
+ScrollReveal().reveal('.yokokara', {
+  duration: 1600,
+  origin: 'left',
+  distance: '50px',
+  reset: true,
+});
+ScrollReveal().reveal('.imi', {
+  duration: 1600,
+  origin: 'left',
+  distance: '50px',
+  reset: true,
+});
+ScrollReveal().reveal('.item', {
+  duration: 1600,
+  origin: 'left',
+  distance: '50px',
+  reset: true,
+});
+ScrollReveal().reveal('img', {
+  duration: 1600,
+  origin: 'left',
+  distance: '50px',
+  reset: true,
+});
