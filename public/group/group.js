febeef5945a7f365877eed0512ecc83eee90a4b8
hgTomoro0726 <tomo.net365@gmail.com>
2021-09-03 10:16:58 +0900

    ロゴ追加

--- public/group/group.js
+++ public/group/group.js
@@ -5,3 +5,9 @@ $('.menu').on('click', function () {
   //上記のコードはsection追加ごとに付け足す
   $('section').fadeToggle();
 });
+ScrollReveal().reveal('.texts_1', {
+  duration: 1600,
+  origin: 'left',
+  distance: '50px',
+  reset: true
+});
