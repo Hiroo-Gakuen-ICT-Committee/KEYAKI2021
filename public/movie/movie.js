70e6a1ed80879ed0e973089efa72a10a82727cf7
hgTomoro0726 <tomo.net365@gmail.com>
2021-09-07 15:24:25 +0900

    ソース管理完了

--- public/movie/movie.js
+++ public/movie/movie.js
@@ -1,7 +1,14 @@
 var pack = prompt("パスワードを入力してください。");
+$('body').addClass('kesu');
+
+
 
 pack_a="neko"
 
+
+
 if (pack == pack_a) {
     console.log("パスワードの認証に成功しました。");
+    $('body').removeClass('kesu');
+
 }
