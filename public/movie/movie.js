da7defd78ce215976659c1a523fa9c028b178992
hgTomoro0726 <tomo.net365@gmail.com>
2021-09-08 10:56:29 +0900

    パスワード考えたやつ消えてくれ

--- public/movie/movie.js
+++ public/movie/movie.js
@@ -11,9 +11,11 @@ var pack_a="neko"
 if (pack == pack_a) {
     console.log("パスワードの認証に成功しました。");
     $('true').removeClass('kesu');
+    $('falese').addClass('kesu');
 } else {
     console.log("パスワードの認証に失敗しました。");
 
     $('fales').removeClass('kesu');
+    $('true').addClass('kesu');
     
 }
