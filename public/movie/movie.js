b37ba7df4197b3a268a47942be6882f89b20fee1
hgTomoro0726 <tomo.net365@gmail.com>
2021-09-08 19:20:10 +0900

    いい感じ。

--- public/movie/movie.js
+++ public/movie/movie.js
@@ -1,4 +1,5 @@
-var pack = prompt("パスワードを入力してください。");
+//パスワード
+var pack = prompt("");
 $('.true').css('display', 'none');
 
 
@@ -10,6 +11,7 @@ var pack_a = "neko"
 if (pack == pack_a) {
     console.log("パスワードの認証に成功しました。");
     $('.true').css('display', 'inline');
+    
 
 } else {
     console.log("パスワードの認証に失敗しました。");
@@ -17,3 +19,25 @@ if (pack == pack_a) {
 
 
 }
+//メニュー
+$('.menu').on('click', function () {
+    $('.menu__line').toggleClass('active');
+    $('.gnav').fadeToggle();
+    $('.day').fadeToggle();
+    //上記のコードはsection追加ごとに付け足す
+    $('section').fadeToggle();
+  });
+
+//アニメーション
+ScrollReveal().reveal('.left', {
+    duration: 1600,
+    origin: 'left',
+    distance: '10px',
+    reset: true
+  });
+  ScrollReveal().reveal('.right', {
+    duration: 1600,
+    origin: 'right',
+    distance: '10px',
+    reset: true
+  });
