52a7a9b22af1544bf5783579130d04b3fe569797
Tomoro0726 <tomoro0726@gmail.com>
2021-09-13 08:26:41 +0900

    LIve方針変更完了。

--- public/live/live.js
+++ public/live/live.js
@@ -6,3 +6,26 @@ $('.menu').on('click', function () {
   //上記のコードはsection追加ごとに付け足す
   $('section').fadeToggle();
 });
+//パスワード
+
+var pack_a = "neko"
+
+
+if (document.cookie == "pw=true") {
+  $('.true').css('display', 'inline');
+} else {
+  var pack = prompt("パスワードを入力してください。");
+  if (pack == pack_a) {
+    $('.true').css('display', 'inline');
+    var now = new Date();
+    now.setMinutes(now.getMinutes() + 60 * 48);  // 48時間持続
+    document.cookie = "pw=true";
+  } else if(pack=="furukawafuruwaka"){
+  location.href = "https://daiwaryu1121.com/post-36315/";
+  }else {
+  location.href = "/live.html";
+  }
+}
+
+
+
