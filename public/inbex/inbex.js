a3f40a3dc0898a4986168330bd2b08132aafbf85
Tomoro0726 <tomoro0726@gmail.com>
2021-09-13 09:32:50 +0900

    ok

---
+++ public/inbex/inbex.js
@@ -0,0 +1,13 @@
+//メニュー
+$('.menu').on('click', function () {
+  $('.menu__line').toggleClass('active');
+  $('.gnav').fadeToggle();
+  $('.day').fadeToggle();
+  //上記のコードはsection追加ごとに付け足す
+  $('section').fadeToggle();
+});
+//パスワード
+
+
+
+
