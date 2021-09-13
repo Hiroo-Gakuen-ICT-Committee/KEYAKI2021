1cf10d7cc01fc457552b04fcd3e5c53ce9f8f5f8
Tomoro0726 <tomoro0726@gmail.com>
2021-09-13 14:49:55 +0900

    よろし

--- public/all2.js
+++ public/all2.js
@@ -4,13 +4,13 @@ live_web = true
 
 //配信ページ
 if (live_web == true) {
-    $('h1').remove();
-    $('.hero').css('background-image', 'none');
-    $('body').css('background-color', '#000000');
-    $('.menu__line').css('background', '#c2c7c2');
-    $('.texts_1').css('background', '#000000');
-    $('.start').css('display', 'inline');
-    $('.texts_1').css('margin-top', '0px');
+  $('h1').remove();
+  $('.hero').css('background-image', 'none');
+  $('body').css('background-color', '#000000');
+  $('.menu__line').css('background', '#c2c7c2');
+  $('.texts_1').css('background', '#000000');
+  //$('.start').css('display', 'inline');
+  $('.texts_1').css('margin-top', '0px');
 
 
 
